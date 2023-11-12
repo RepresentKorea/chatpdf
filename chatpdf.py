09bb19eb55a7d8750a4799988811cf1000a05d51
RepresentKorea <136456298+RepresentKorea@users.noreply.github.com>
2023-11-12 16:33:26 +0900

    Update chatpdf.py

--- chatpdf.py
+++ chatpdf.py
@@ -2,8 +2,8 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
